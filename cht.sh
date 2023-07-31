704693df07c5162ad31508d2ff5b276a6ea4fb5c
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-08-01 01:55:14 +0530

    Added all scripts

---
+++ cht.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+## this script uses cht.sh to search for queries
+
+languages=`echo "python java javascript c cpp csharp rust lua" | tr ' ' '\n'`
+core_utils=`echo "xargs find mv sed awk fzf man grep bash zsh fish" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf -i`
+read -p "Query: " query
+
+if printf "$languages" | grep -qs $selected; then
+    bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+    bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+fi
